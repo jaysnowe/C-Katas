6047909be8d84d6379ee607e7783eb47617ec128
Jay Snowe <jaysnowe.pgh@gmail.com>
2020-09-22 11:10:14 -0400

    simple function that sums two numbers and returns to main

---
+++ Function_SumTwoNumbers/Function_SumTwoNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SumFunction
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me the first number to sum.");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please give me the second number to sum.");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine($"The sum of the two numbers is {Sum(firstNum, secondNum)}");
+
+        }
+        public static int Sum(int firstNum, int secondNum)
+        {
+            int sum = firstNum + secondNum;
+            return sum;
+        }
+    }
+}
+
+
+
+
