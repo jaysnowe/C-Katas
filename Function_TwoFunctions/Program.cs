bb978b72a12123298cd33c9e2d626f32d86889b0
Jay Snowe <jaysnowe.pgh@gmail.com>
2020-09-23 16:49:38 -0400

    function that takes in two numbers and returns quotient. Utilizes a calculate and print function

---
+++ Function_TwoFunctions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Function_TwoFunctions
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double quotient = 0;
+            Console.WriteLine("Please give me the dividend.");
+            double firstNum = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please give me the divisor.");
+            double secondNum = Convert.ToDouble(Console.ReadLine());
+
+            quotient = CalcQuotient(firstNum, secondNum);
+            PrintQuotient(quotient);
+        }
+        public static double CalcQuotient(double firstNum, double secondNum)
+        {
+            double quotient = firstNum / secondNum;
+            return quotient;
+        }
+        public static void PrintQuotient(double quotient)
+        {
+            Console.WriteLine($"Your quotient is {quotient}");
+        }
+    }
+}
+
+
+
+
